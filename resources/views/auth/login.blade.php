b48daea8cc1cd72aa584a8d446ccc0880b5ceb24
Roberto Maciel <lmaciel1303@gmail.com>
2020-08-27 12:33:57 -0500

    Make login with alias/username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,12 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="alias" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="alias" type="text" class="form-control @error('alias') is-invalid @enderror" name="alias" value="{{ old('alias') }}" required autocomplete="alias" autofocus>
 
-                                @error('email')
+                                @error('alias')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="alias" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="alias" type="text" class="form-control @error('alias') is-invalid @enderror" name="alias" value="{{ old('name') }}" required autocomplete="alias" autofocus>
+
+                                @error('alias')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
