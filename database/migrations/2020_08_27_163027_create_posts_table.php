0286dd8222b5c9c6f64cd9ca281f1eca6415c763
Roberto Maciel <lmaciel1303@gmail.com>
2020-08-27 11:48:07 -0500

    Update fields in Category Migration

--- database/migrations/2020_08_27_163027_create_posts_table.php
+++ database/migrations/2020_08_27_163027_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->timestamps();
 
             // Foreign Keys
